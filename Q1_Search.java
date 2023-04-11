648d0bd4874f2d958c79e553d0845dce18274cd4
ParthSorathiya1210 <122281080+ParthSorathiya1210@users.noreply.github.com>
2023-04-11 22:16:29 +0530

    Add files via upload

--- Q1_Search.java
+++ Q1_Search.java
@@ -1,190 +1,150 @@
 import java.util.Scanner;
-class Node
-{
+
+class Node {
     int data;
     Node next;
 
-    Node(int data)
-    {
+    Node(int data) {
         this.data = data;
         this.next = null;
     }
 }
 
-class linkedList
-{
+class linkedList {
     Node head = null;
     Node temp = null;
     int size = 0;
-    void create(int data)
-    {
+
+    void create(int data) {
         Node newNode = new Node(data);
-        if(head == null)
-        {
+        if (head == null) {
             head = temp = newNode;
-        }
-        else
-        {
+        } else {
             temp.next = newNode;
             temp = newNode;
         }
         size++;
     }
-    void display()
-    {
+
+    void display() {
         temp = head;
-        while(temp != null)
-        {
-            System.out.print(temp.data+" ");
+        while (temp != null) {
+            System.out.print(temp.data + " ");
             temp = temp.next;
         }
         System.out.println();
     }
-    void insertBegin(int data)
-    {
+
+    void insertBegin(int data) {
         Node newNode = new Node(data);
-        if(head == null)
-        {
+        if (head == null) {
             head = newNode;
-        }
-        else
-        {
+        } else {
             newNode.next = head;
             head = newNode;
         }
         size++;
     }
-    void insertEnd(int data)
-    {
+
+    void insertEnd(int data) {
         Node newNode = new Node(data);
-        if(head == null)
-        {
+        if (head == null) {
             head = newNode;
-        }
-        else
-        {
+        } else {
             temp = head;
-            while(temp.next != null)
-            {
+            while (temp.next != null) {
                 temp = temp.next;
             }
             temp.next = newNode;
         }
         size++;
     }
-    void insertPosition(int data,int pos)
-    {
-        if(pos == 1) insertBegin(data);
-        else if(pos<=size+1)
-        {
+
+    void insertPosition(int data, int pos) {
+        if (pos == 1)
+            insertBegin(data);
+        else if (pos <= size + 1) {
             temp = head;
             int i = 2;
-            while(i<pos)
-            {
+            while (i < pos) {
                 temp = temp.next;
                 i++;
             }
-            if(temp == null)
-            {
+            if (temp == null) {
                 insertEnd(data);
-            }
-            else
-            {
+            } else {
                 Node newNode = new Node(data);
                 newNode.next = temp.next;
                 temp.next = newNode;
                 size++;
             }
-        }
-        else
-        {
-            System.out.println("invalid Position");
-        }
-    }
-    void deleteBegin()
-    {
-        if(head == null)
-        {
-            System.out.println("LinkedList is empty");
-        }
-        else
-        {
-            temp = head;
-            head = head.next;
-            System.out.println("deleted : "+temp.data);
-            size--;
-        }
-    }
-    void deleteEnd()
-    {
-        if(head == null)
-        {
-            System.out.println("LinkedList is empty");
-        }
-        else if(head.next == null)
-        {
-            temp = head;
-            head = null;
-            System.out.println("deleted : "+temp.data);
-            size--;
-        }
-        else
-        {
-            temp = head;
-            Node preTemp = null;
-            while(temp.next!=null)
-            {
-                preTemp = temp;
-                temp = temp.next;
-            }
-            preTemp.next = null;
-            System.out.println("deleted : "+temp.data);
-            size--;
-        }       
-    }
-    void deletePosition(int pos)
-    {
-        if(pos == 1)
-        {
-            deleteBegin();
-        }
-        else if(pos<=size)
-        {
-            int i = 1;
-            temp = head;
-            Node PreTemp = null;
-            while(i<pos)
-            {
-                PreTemp = temp;
-                temp = temp.next;
-                i++;
-            }
-            if(temp == null)
-            {
-                deleteEnd();
-            }
-            else
-            {
-                PreTemp.next = temp.next;
-                System.out.println("deleted : "+temp.data);
-                size--;
-            }
-        }
-        else
-        {
+        } else {
             System.out.println("invalid Position");
         }
     }
 
-    int search(int data)
-    {
+    void deleteBegin() {
+        if (head == null) {
+            System.out.println("LinkedList is empty");
+        } else {
+            temp = head;
+            head = head.next;
+            System.out.println("deleted : " + temp.data);
+            size--;
+        }
+    }
+
+    void deleteEnd() {
+        if (head == null) {
+            System.out.println("LinkedList is empty");
+        } else if (head.next == null) {
+            temp = head;
+            head = null;
+            System.out.println("deleted : " + temp.data);
+            size--;
+        } else {
+            temp = head;
+            Node preTemp = null;
+            while (temp.next != null) {
+                preTemp = temp;
+                temp = temp.next;
+            }
+            preTemp.next = null;
+            System.out.println("deleted : " + temp.data);
+            size--;
+        }
+    }
+
+    void deletePosition(int pos) {
+        if (pos == 1) {
+            deleteBegin();
+        } else if (pos <= size) {
+            int i = 1;
+            temp = head;
+            Node PreTemp = null;
+            while (i < pos) {
+                PreTemp = temp;
+                temp = temp.next;
+                i++;
+            }
+            if (temp == null) {
+                deleteEnd();
+            } else {
+                PreTemp.next = temp.next;
+                System.out.println("deleted : " + temp.data);
+                size--;
+            }
+        } else {
+            System.out.println("invalid Position");
+        }
+    }
+
+    int search(int data) {
         int i = 0;
         temp = head;
-        while(temp.next!=null)
-        {
+        while (temp.next != null) {
             i++;
-            if(data == temp.data)
-            {
+            if (data == temp.data) {
                 return i;
             }
             temp = temp.next;
@@ -193,30 +153,25 @@ class linkedList
     }
 }
 
-public class Q1_Search 
-{
-    public static void main(String args[])
-    {
+public class Q1_Search {
+    public static void main(String args[]) {
         Scanner ip = new Scanner(System.in);
         System.out.println("Create the Linked List :-");
         linkedList List1 = new linkedList();
-        List1.create(1);
-        List1.create(5);
-        List1.create(7);
-        List1.create(3);
-        List1.create(8);
-        List1.create(2);
-        List1.create(3);
+        List1.insertBegin(1);
+        List1.insertPosition(5,2);
+        List1.insertPosition(7,3);
+        List1.insertPosition(3,4);
+        List1.insertEnd(8);
+        List1.insertEnd(2);
+        List1.insertEnd(3);
         List1.display();
         int data = 7;
         int idx = List1.search(data);
-        if(idx == 0)
-        {
+        if (idx == 0) {
             System.out.println("Element is not in the Linklist");
-        }
-        else
-        {
-            System.out.println("index of "+data+" is : "+idx);
+        } else {
+            System.out.println("index of " + data + " is : " + idx);
         }
     }
 }
